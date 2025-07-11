5b146ec49e0d71ebf299660228f9d955780065c6
Gayan65 <gayan65728@yahoo.com>
2025-07-11 15:35:25 +0300

    Con-rend add

--- src/app/employees/page.tsx
+++ src/app/employees/page.tsx
@@ -1,12 +1,12 @@
 import EmployeesTable from "@/components/EmployeesTable";
+import { stackServerApp } from "@/stack";
+import { SignIn } from "@stackframe/stack";
 import React from "react";
 
-const page = () => {
-    return (
-        <div>
-            <EmployeesTable />
-        </div>
-    );
-};
+async function page() {
+    const user = await stackServerApp.getUser();
+    const app = stackServerApp.urls;
+    return <div>{user ? <EmployeesTable /> : <SignIn />}</div>;
+}
 
 export default page;
