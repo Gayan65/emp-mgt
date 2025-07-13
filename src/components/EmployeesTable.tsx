1fe34ca029c31b58629897586c6b46ef928f1782
Gayan65 <gayan65728@yahoo.com>
2025-07-13 13:20:24 +0300

    add combo box

--- src/components/EmployeesTable.tsx
+++ src/components/EmployeesTable.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
     Table,
     TableBody,
@@ -6,6 +8,10 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table";
+import { Input } from "./ui/input";
+import { Search } from "lucide-react";
+import { Combobox } from "./ui/combo-box";
+import { useState } from "react";
 
 const employees = [
     {
@@ -16,31 +22,44 @@ const employees = [
 ];
 
 export function EmployeesTable() {
+    const [selectedCategory, setSelectedCategory] = useState("");
     return (
-        <Table>
-            <TableHeader>
-                <TableRow>
-                    <TableHead>Employee Id</TableHead>
-                    <TableHead>Employee Name</TableHead>
-                    <TableHead>Position</TableHead>
-                    <TableHead className="text-right">Amount</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {employees.map((employee) => (
-                    <TableRow key={employee.id}>
-                        <TableCell>{employee.id}</TableCell>
-                        <TableCell>{employee.name}</TableCell>
-                        <TableCell>{employee.position}</TableCell>
-                        <TableCell className="text-right">
-                            <div className="flex justify-end space-x-4">
-                                <h1>Edit button</h1>
-                                <h1>Delete button</h1>
-                            </div>
-                        </TableCell>
+        <div className="w-full">
+            <div className="flex items-center gap-2 py-4">
+                <div className="relative max-w--sm w-full">
+                    <Input placeholder="Filter employees" className="pl-10" />
+                    <Search className="absolute h-4 left-3 top-1/2 transform -translate-y-1/2" />
+                </div>
+                <Combobox
+                    value={selectedCategory}
+                    onChange={(val) => setSelectedCategory(val)}
+                />
+            </div>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Employee Id</TableHead>
+                        <TableHead>Employee Name</TableHead>
+                        <TableHead>Position</TableHead>
+                        <TableHead className="text-right">Amount</TableHead>
                     </TableRow>
-                ))}
-            </TableBody>
-        </Table>
+                </TableHeader>
+                <TableBody>
+                    {employees.map((employee) => (
+                        <TableRow key={employee.id}>
+                            <TableCell>{employee.id}</TableCell>
+                            <TableCell>{employee.name}</TableCell>
+                            <TableCell>{employee.position}</TableCell>
+                            <TableCell className="text-right">
+                                <div className="flex justify-end space-x-4">
+                                    <h1>Edit button</h1>
+                                    <h1>Delete button</h1>
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
     );
 }
